3fb582c1cf0eda88ec022154263134b6ca446a2c
John-Nagle <nagle@sitetruth.com>
2022-06-06 10:46:24 -0700

    Initial commit

---
+++ src/gui.rs
@@ -0,0 +1,88 @@
+//
+//  The 2D GUI
+//
+use crate::{EguiAssets, EguiExampleData};
+use egui::{menu, Frame, TextureId};
+use rend3::Renderer;
+use rend3_egui::EguiRenderRoutine;
+use std::sync::Arc;
+
+/// GUI utility functions
+
+/// Load an icon at compile time. Image is built into executable.
+pub fn load_canned_icon(
+    image_bytes: &[u8],
+    egui_routine: &mut EguiRenderRoutine,
+    renderer: &Arc<Renderer>,
+) -> TextureId {
+    //Images
+    let image_image = image::load_from_memory(image_bytes).unwrap();
+    let image_rgba = image_image.as_rgba8().unwrap().clone().into_raw();
+    use image::GenericImageView;
+    let dimensions = image_image.dimensions();
+    let format = wgpu::TextureFormat::Rgba8UnormSrgb;
+    //  Create and return texture
+    rend3_egui::EguiRenderRoutine::create_egui_texture(
+        &mut egui_routine.internal,
+        renderer,
+        format,
+        &image_rgba,
+        dimensions,
+        Some("Canned icon"),
+    )
+}
+
+/// Update the GUI. Called on each frame.
+pub fn update_gui(assets: &EguiAssets, data: &mut EguiExampleData, show_menus: bool) -> bool {
+    ////let data = example.data.as_mut().unwrap();
+    // Insert egui commands here
+    let ctx = data.platform.context();
+    //  Top menu bar
+    let mut inuse = false;                  // true if GUI in use
+    if show_menus {
+        if egui::TopBottomPanel::top("menu_bar").show(&ctx, |ui| {
+            menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    let response = ui.button("Open");
+                    if response.clicked() {
+                        if let Some(path) = rfd::FileDialog::new()
+                            .set_title("Viewer session file to play back")
+                            .add_filter("json", &["json"])
+                            .pick_file()
+                        {
+                            let picked_path = Some(path.display().to_string());
+                            println!("File picked: {}", picked_path.unwrap());
+                        }
+                    }
+                    if response.hovered() { inuse = true; }     // pointer is over this button
+                });
+                ui.menu_button("Help", |ui| {
+                    let response = ui.button("Help");
+             	    if response.clicked() {
+                        webbrowser::open("https://www.rust-lang.org").expect("failed to open URL");
+                    }
+                    if response.hovered() { inuse = true; }
+                });
+            });
+        }).response.hovered() { inuse = true };
+        //  Bottom button panel
+        if egui::TopBottomPanel::bottom("bottom_panel")
+            .frame(Frame::none().fill(egui::Color32::TRANSPARENT))
+            .show(&ctx, |ui| {
+                if ui
+                    .add(
+                        egui::widgets::ImageButton::new(
+                            (*assets).rust_logo,
+                            egui::Vec2::splat(64.0),
+                        )
+                        .frame(false),
+                    )
+                    .clicked()
+                {
+                    println!("Clicked on Rust button");
+                }
+            }).response.hovered() { inuse = true };
+        
+    }
+    inuse                       // true if menus are in use
+}
---
+++ src/main.rs
@@ -0,0 +1,314 @@
+mod gui;
+use std::sync::Arc;
+use gui::update_gui;
+
+
+pub struct EguiExampleData {
+    _object_handle: rend3::types::ObjectHandle,
+    material_handle: rend3::types::MaterialHandle,
+    _directional_handle: rend3::types::DirectionalLightHandle,
+
+    egui_routine: rend3_egui::EguiRenderRoutine,
+    platform: egui_winit_platform::Platform,
+    start_time: instant::Instant,
+    last_interaction_time: instant::Instant,
+}
+
+impl EguiExampleData {
+    /// Call this for anything that indicates the GUI should be awakened to show menus.
+    pub fn wake_up_gui(&mut self) {
+        self.last_interaction_time = instant::Instant::now();
+    }
+}
+
+const SAMPLE_COUNT: rend3::types::SampleCount = rend3::types::SampleCount::One;
+
+/// Assets used in displaying the GUI.
+#[derive(Default)]
+pub struct EguiAssets {
+    rust_logo: egui::TextureId,
+}
+
+#[derive(Default)]
+pub struct EguiExample {
+    data: Option<EguiExampleData>,
+    assets: EguiAssets,
+
+}
+impl rend3_framework::App for EguiExample {
+    const HANDEDNESS: rend3::types::Handedness = rend3::types::Handedness::Left;
+
+    fn sample_count(&self) -> rend3::types::SampleCount {
+        SAMPLE_COUNT
+    }
+
+    fn setup(
+        &mut self,
+        window: &winit::window::Window,
+        renderer: &Arc<rend3::Renderer>,
+        _routines: &Arc<rend3_framework::DefaultRoutines>,
+        surface_format: rend3::types::TextureFormat,
+    ) {
+        let window_size = window.inner_size();
+
+        // Create the egui render routine
+        let mut egui_routine = rend3_egui::EguiRenderRoutine::new(
+            renderer,
+            surface_format,
+            rend3::types::SampleCount::One,
+            window_size.width,
+            window_size.height,
+            window.scale_factor() as f32,
+        );
+
+        // Create mesh and calculate smooth normals based on vertices
+        let mesh = create_mesh();
+
+        // Add mesh to renderer's world.
+        //
+        // All handles are refcounted, so we only need to hang onto the handle until we
+        // make an object.
+        let mesh_handle = renderer.add_mesh(mesh);
+
+        // Add PBR material with all defaults except a single color.
+        let material = rend3_routine::pbr::PbrMaterial {
+            albedo: rend3_routine::pbr::AlbedoComponent::Value(glam::Vec4::new(0.0, 0.5, 0.5, 1.0)),
+            transparency: rend3_routine::pbr::Transparency::Blend,
+            ..rend3_routine::pbr::PbrMaterial::default()
+        };
+        let material_handle = renderer.add_material(material);
+
+        // Combine the mesh and the material with a location to give an object.
+        let object = rend3::types::Object {
+            mesh_kind: rend3::types::ObjectMeshKind::Static(mesh_handle),
+            material: material_handle.clone(),
+            transform: glam::Mat4::IDENTITY,
+        };
+
+        // Creating an object will hold onto both the mesh and the material
+        // even if they are deleted.
+        //
+        // We need to keep the object handle alive.
+        let _object_handle = renderer.add_object(object);
+
+        let camera_pitch = std::f32::consts::FRAC_PI_4;
+        let camera_yaw = -std::f32::consts::FRAC_PI_4;
+        // These values may seem arbitrary, but they center the camera on the cube in
+        // the scene
+        let camera_location = glam::Vec3A::new(5.0, 7.5, -5.0);
+        let view = glam::Mat4::from_euler(glam::EulerRot::XYZ, -camera_pitch, -camera_yaw, 0.0);
+        let view = view * glam::Mat4::from_translation((-camera_location).into());
+
+        // Set camera location data
+        renderer.set_camera_data(rend3::types::Camera {
+            projection: rend3::types::CameraProjection::Perspective { vfov: 60.0, near: 0.1 },
+            view,
+        });
+
+        // Create a single directional light
+        //
+        // We need to keep the directional light handle alive.
+        let _directional_handle = renderer.add_directional_light(rend3::types::DirectionalLight {
+            color: glam::Vec3::ONE,
+            intensity: 10.0,
+            // Direction will be normalized
+            direction: glam::Vec3::new(-1.0, -4.0, 2.0),
+            distance: 400.0,
+        });
+
+        // Create the winit/egui integration, which manages our egui context for us.
+        let platform = egui_winit_platform::Platform::new(egui_winit_platform::PlatformDescriptor {
+            physical_width: window_size.width as u32,
+            physical_height: window_size.height as u32,
+            scale_factor: window.scale_factor(),
+            font_definitions: egui::FontDefinitions::default(),
+            style: Default::default(),
+        });
+
+        //  Icon loading
+        let image_bytes = include_bytes!("images/rust-logo-128x128-blk.png");
+        self.assets.rust_logo = gui::load_canned_icon(image_bytes, &mut egui_routine, renderer);
+
+        let start_time = instant::Instant::now();
+        let last_interaction_time = instant::Instant::now();
+
+        self.data = Some(EguiExampleData {
+            _object_handle,
+            material_handle,
+            _directional_handle,
+            egui_routine,
+            platform,
+            start_time,
+            last_interaction_time,
+        });
+    }
+
+    fn handle_event(
+        &mut self,
+        window: &winit::window::Window,
+        renderer: &Arc<rend3::Renderer>,
+        routines: &Arc<rend3_framework::DefaultRoutines>,
+        base_rendergraph: &rend3_routine::base::BaseRenderGraph,
+        surface: Option<&Arc<rend3::types::Surface>>,
+        resolution: glam::UVec2,
+        event: rend3_framework::Event<'_, ()>,
+        control_flow: impl FnOnce(winit::event_loop::ControlFlow),
+    ) {
+        let data = self.data.as_mut().unwrap();
+
+        // Pass the winit events to the platform integration.
+        data.platform.handle_event(&event);
+
+        match event {
+            rend3_framework::Event::RedrawRequested(..) => {
+                data.platform.update_time(data.start_time.elapsed().as_secs_f64());
+                data.platform.begin_frame();
+
+                // Insert egui commands here
+                const MENU_DISPLAY_SECS: u64 = 5;   // hide menus after this much time
+                let show_menus = data.last_interaction_time.elapsed().as_secs() < 	MENU_DISPLAY_SECS;
+                let inuse = update_gui(&self.assets, data, show_menus);
+                if inuse { data.wake_up_gui(); }
+                let egui::FullOutput {
+                    shapes, textures_delta, platform_output, ..
+                } = data.platform.end_frame(Some(window));
+                if !platform_output.events.is_empty() {
+                    data.wake_up_gui(); // reset GUI idle time.
+                    println!("Platform events: {:?}, {} shapes.", platform_output.events, shapes.len());   // ***TEMP***
+                }
+                
+                let paint_jobs = data.platform.context().tessellate(shapes);
+
+                let input = rend3_egui::Input {
+                    clipped_meshes: &paint_jobs,
+                    textures_delta,
+                    context: data.platform.context(),
+                };
+
+                // Get a frame
+                let frame = rend3::util::output::OutputFrame::Surface {
+                    surface: Arc::clone(surface.unwrap()),
+                };
+
+                // Ready up the renderer
+                let (cmd_bufs, ready) = renderer.ready();
+
+                // Lock the routines
+                let pbr_routine = rend3_framework::lock(&routines.pbr);
+                let tonemapping_routine = rend3_framework::lock(&routines.tonemapping);
+
+                // Build a rendergraph
+                let mut graph = rend3::graph::RenderGraph::new();
+
+                // Add the default rendergraph without a skybox
+                base_rendergraph.add_to_graph(
+                    &mut graph,
+                    &ready,
+                    &pbr_routine,
+                    None,
+                    &tonemapping_routine,
+                    resolution,
+                    SAMPLE_COUNT,
+                    glam::Vec4::ZERO,
+                    glam::Vec4::new(0.10, 0.05, 0.10, 1.0), // Nice scene-referred purple
+                );
+
+                // Add egui on top of all the other passes
+                let surface = graph.add_surface_texture();
+                data.egui_routine.add_to_graph(&mut graph, input, surface);
+
+                // Dispatch a render using the built up rendergraph!
+                graph.execute(renderer, frame, cmd_bufs, &ready);
+
+                control_flow(winit::event_loop::ControlFlow::Poll);
+            }
+            rend3_framework::Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            rend3_framework::Event::WindowEvent { event, .. } => match event {
+                winit::event::WindowEvent::Resized(size) => {
+                    data.egui_routine
+                        .resize(size.width, size.height, window.scale_factor() as f32);
+                }
+                winit::event::WindowEvent::CloseRequested => {
+                    control_flow(winit::event_loop::ControlFlow::Exit);
+                }
+                winit::event::WindowEvent::Focused(gained) => {
+                    if gained { data.wake_up_gui(); } // make menus reappear on focus
+                }
+                winit::event::WindowEvent::CursorEntered{ .. } => {
+                    data.wake_up_gui();                     // either entering or leaving makes menus reappear
+                }
+                winit::event::WindowEvent::CursorLeft{ .. } => {
+                    data.wake_up_gui();
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
+fn main() {
+    let app = EguiExample::default();
+    rend3_framework::start(
+        app,
+        winit::window::WindowBuilder::new()
+            .with_title("egui")
+            .with_maximized(true),
+    )
+}
+
+fn vertex(pos: [f32; 3]) -> glam::Vec3 {
+    glam::Vec3::from(pos)
+}
+
+fn create_mesh() -> rend3::types::Mesh {
+    let vertex_positions = [
+        // far side (0.0, 0.0, 1.0)
+        vertex([-1.0, -1.0, 1.0]),
+        vertex([1.0, -1.0, 1.0]),
+        vertex([1.0, 1.0, 1.0]),
+        vertex([-1.0, 1.0, 1.0]),
+        // near side (0.0, 0.0, -1.0)
+        vertex([-1.0, 1.0, -1.0]),
+        vertex([1.0, 1.0, -1.0]),
+        vertex([1.0, -1.0, -1.0]),
+        vertex([-1.0, -1.0, -1.0]),
+        // right side (1.0, 0.0, 0.0)
+        vertex([1.0, -1.0, -1.0]),
+        vertex([1.0, 1.0, -1.0]),
+        vertex([1.0, 1.0, 1.0]),
+        vertex([1.0, -1.0, 1.0]),
+        // left side (-1.0, 0.0, 0.0)
+        vertex([-1.0, -1.0, 1.0]),
+        vertex([-1.0, 1.0, 1.0]),
+        vertex([-1.0, 1.0, -1.0]),
+        vertex([-1.0, -1.0, -1.0]),
+        // top (0.0, 1.0, 0.0)
+        vertex([1.0, 1.0, -1.0]),
+        vertex([-1.0, 1.0, -1.0]),
+        vertex([-1.0, 1.0, 1.0]),
+        vertex([1.0, 1.0, 1.0]),
+        // bottom (0.0, -1.0, 0.0)
+        vertex([1.0, -1.0, 1.0]),
+        vertex([-1.0, -1.0, 1.0]),
+        vertex([-1.0, -1.0, -1.0]),
+        vertex([1.0, -1.0, -1.0]),
+    ];
+
+    let index_data: &[u32] = &[
+        0, 1, 2, 2, 3, 0, // far
+        4, 5, 6, 6, 7, 4, // near
+        8, 9, 10, 10, 11, 8, // right
+        12, 13, 14, 14, 15, 12, // left
+        16, 17, 18, 18, 19, 16, // top
+        20, 21, 22, 22, 23, 20, // bottom
+    ];
+
+    rend3::types::MeshBuilder::new(vertex_positions.to_vec(), rend3::types::Handedness::Left)
+        .with_indices(index_data.to_vec())
+        .build()
+        .unwrap()
+}
+
