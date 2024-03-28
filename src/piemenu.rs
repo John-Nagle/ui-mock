9a92090499bb532caaa693c06cd732e30218ceee
John-Nagle <nagle@sitetruth.com>
2024-03-28 14:52:16 -0700

    Displaying pie menu

--- src/piemenu.rs
+++ src/piemenu.rs
@@ -10,12 +10,13 @@
 //  Animats
 //  March 2024
 //
-use core::ops::Index;
+////use core::ops::Index;
 use egui::{Response, Ui, WidgetText};
 use std::f32::consts::PI;
 //  Always write TextureId, Vec2, Rect fully qualified to avoid name confusion.
 
 const LINE_WIDTH: f32 = 2.0;            // line width for drawing
+const TEXT_POS_RADIUS_FRACT: f32 = 0.75;    // how far out to put the text (0..1)
 
 /// PieMenu -- N-choice circular menu
 //  The persistent part.
@@ -34,6 +35,8 @@ pub struct PieMenu {
     hover_color: egui::Color32,
     /// Hover help text
     hover_text: WidgetText,
+    /// Cut vectors, one per button text entry
+    cut_vectors: Vec<egui::Vec2>,
 }
 
 impl PieMenu {
@@ -47,6 +50,9 @@ impl PieMenu {
         hover_color: egui::Color32,
         hover_text: impl Into<WidgetText>,
     ) -> Self {
+        //  Direction vector for each dividing line
+        let cut_vector = |n:usize| egui::Vec2::new((2.0*PI/button_text.len() as f32 * (n as f32)).sin(), 
+            (2.0*PI/button_text.len() as f32 * (n as f32)).cos());
         Self {
             radius,
             center_radius,
@@ -55,72 +61,10 @@ impl PieMenu {
             background_color,	
             hover_color,
             hover_text: hover_text.into(),
+            cut_vectors: (0..button_text.len()).map(|n| cut_vector(n)).collect(),
         }
     }
 
-    //// Decode the click into the user action -- Left, Right, Up, Down, Center, or None.
-    //// Users of this widget must call this on Response to find out what the user is asking for.
-    /*
-    pub fn decode_response(&self, response: &Response) -> NavAction {
-        let response = response.interact(egui::Sense::click_and_drag());    // must sense 'dragged' to sense held down.
-        if response.dragged() {
-            if let Some(interact_pos) = response.hover_pos() {
-                //  Compute position relative to center of button.
-                //  Do case analysis for left, right, center, up, down.
-                let to_vec2 = |p: egui::Pos2| egui::Vec2::new(p.x, p.y); // why not just use one 2d point/vector type?
-                let center = (to_vec2(response.rect.min) + to_vec2(response.rect.max)) * 0.5; // Average for center coords
-                let rel_pos = to_vec2(interact_pos) - center; // cursor position relative to center of button rect.
-                if rel_pos.length() < self.center_button_size {
-                    NavAction::Center // inside center button
-                } else if rel_pos.x.abs() > rel_pos.y.abs() {
-                    // if X dominates
-                    if rel_pos.x > 0.0 {
-                        NavAction::Right
-                    } else {
-                        NavAction::Left
-                    }
-                } else if rel_pos.y < 0.0 {
-                    NavAction::Up
-                } else {
-                    NavAction::Down
-                } // < 0 is upwards. Tradition.
-            } else {
-                NavAction::None //  Must not be in rectangle.
-            }
-        } else {
-            NavAction::None // nothing pushed
-        }
-    }
-
-    /// Draw the appropriate pressed arrow.
-    fn draw_pressed_arrow(&self, ui: &mut Ui, response: Response) -> Response {
-        let nav_action = self.decode_response(&response);
-        //  Which way to point arrow.
-        const ARROW_ROTS: [f32; 6] = [
-            0.0,      // None
-            PI * 1.5, // Up
-            PI * 0.5, // Down
-            PI * 1.0, // Left
-            PI * 0.0, // Right
-            0.0,      // center
-        ];
-        match nav_action {
-            NavAction::None => {}     // no press
-            NavAction::Center => {              // center press
-                self.center_button.paint_at(ui, response.rect);
-            }
-            _ => {
-                //  Arrow press
-                let arrow_rot = ARROW_ROTS[nav_action as usize];
-                // Draw the arrow if pressed
-                self.arrow.clone()
-                .rotate(arrow_rot, egui::Vec2::new(0.5, 0.5))
-                .paint_at(ui, response.rect);
-            }
-        }
-        response    // pass through response
-    }
-    */
     
     /// Radius of click menu.
     pub fn get_radius(&self) -> f32 {
@@ -141,10 +85,14 @@ impl egui::Widget for &mut PieMenu {
         //  Inner circle
         painter.circle(center, self.center_radius, egui::Color32::TRANSPARENT, stroke);
         let pie_cut = |v: egui::Vec2| { painter.line_segment([center + v*self.center_radius, center + v*self.radius], stroke); };
-        pie_cut(egui::Vec2::new(0.0, 1.0));
-        pie_cut(egui::Vec2::new(0.0, -1.0));
-        pie_cut(egui::Vec2::new(1.0, 0.0));
-        pie_cut(egui::Vec2::new(-1.0, 0.0));
+        
+        //  Draw the dividing lines and labels
+        for (dir, text) in self.cut_vectors.iter().zip(&self.button_text) {
+            pie_cut(*dir);   // draw the line
+            let font_id = egui::FontId::default();           // for now
+            let text_pos = center + (*dir)*(self.center_radius * (1.0 - TEXT_POS_RADIUS_FRACT) + self.radius*TEXT_POS_RADIUS_FRACT);
+            painter.text(text_pos, egui::Align2::CENTER_CENTER, text.text().to_string(), font_id, egui::Color32::WHITE);
+        }
         response
     }
 }
